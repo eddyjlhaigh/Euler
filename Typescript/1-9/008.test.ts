6376e803e76271bf5409c301ba990be934a50242
eddyjlhaigh <eddyhaigh@gmail.com>
2021-01-30 16:06:57 +0000

    Add solution to problem 008 in typescript

---
+++ Typescript/1-9/008.test.ts
@@ -0,0 +1,9 @@
+import problem008 from './008';
+
+test('Euler Example Passes', () => {
+  expect(problem008(4)).toEqual(5832);
+});
+
+test('Euler Problem 008 Passes', () => {
+  expect(problem008(13)).toEqual(23514624000); 
+});
