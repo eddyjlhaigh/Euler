6598384cd21154fb0a8e6ee56ee9f90f1039d0bc
eddyjlhaigh <eddyhaigh@gmail.com>
2021-01-31 16:15:09 +0000

    Add lisp dir (for Clojure)

---
+++ Typescript/1-9/009.test.ts
@@ -0,0 +1,9 @@
+import problem009 from './009';
+
+test('Euler Example Passes', () => {
+  expect(problem009(4)).toEqual(5832);
+});
+
+// test('Euler Problem 009 Passes', () => {
+//   expect(problem009(13)).toEqual(23514624000); 
+// });
